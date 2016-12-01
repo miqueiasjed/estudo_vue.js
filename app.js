d38d352085f91b7b0ece3499931b8cfbfdb8f1ec
Miquéias Mesquita <Miquias Mesquita>
2016-12-01 20:25:25 -0300

    Criando estrutra para mostrar: Listar e criar view

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var app = new Vue({
     el: "#app",
     data:{
+        /*Mostra o título*/
         title: "Contas a pagar",
+
+        /*Armazena os menus*/
         menus:[
             {id:0, name: "Listar contas"},
             {id:1, name: "Criar conta"}
         ],
+
+        /*Armazena o status do menu*/
+        activedView: 0,
+
+        /*Mostra as contas a pagar*/
         bills:[
             {data_due: '20/08/2016', name: 'Conta de luz', value: 70.99, done: 1},
             {data_due: '21/08/2016', name: 'Conta de água', value: 55.99, done: 0},
@@ -16,6 +24,8 @@ var app = new Vue({
             {data_due: '26/08/2016', name: 'Gasolina', value: 200.99, done: 0}
         ]
     },
+
+    /*Calcula as contas a pagar*/
     computed:{
         status: function () {
             var count = 0;
@@ -27,9 +37,11 @@ var app = new Vue({
             return !count ? "Nenhuma conta a pagar" : "Existem " + count + " contas a serem pagas";
             }
         },
+    
+    /*Executa o método que troca o status do menu*/
         methods:{
-            showView: function ($number) {
-                console.log($number);
+            showView: function (id) {
+                this.activedView = id;
             }
         }
 });
